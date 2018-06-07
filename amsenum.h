7720cf845bd3a7fcecd2c4c7146104976e7cedc1
youxiruo <longlong0967@163.com>
2018-06-07 13:43:16 +0800

    提交amsenum.h‘

---
+++ amsenum.h
@@ -0,0 +1,19 @@
+//zry add for cs 201806
+#ifdef cs
+enum amsOperateTargetType
+{
+	AMS_OPERATE_TARGET_RSVD						=0,
+
+	AMS_OPERATE_TARGET_ROBOT                    =1,
+	AMS_OPERATE_TARGET_TELLER                   =2,
+	
+	AMS_OPERATE_TARGET_MBPHONE                  =3,
+
+	AMS_OPERATE_TARGET_PC                       =4,
+
+	AMS_OPERATE_TARGET_MAX,
+}
+
+
+#endif
+
